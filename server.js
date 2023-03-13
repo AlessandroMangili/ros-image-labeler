a175603764baef6b5aa6c81ff217f389f89e6ec5
AlessandroMangili <alessandro.mangili1@studenti.unimi.it>
2023-03-13 16:15:59 +0100

    Add socket.io and move function call on button click
    Signed-off-by: AlessandroMangili <alessandro.mangili1@studenti.unimi.it>

--- server.js
+++ server.js
@@ -1,26 +1,46 @@
 const express = require('express');
 const fs = require('fs');
 const util = require('util');
-const { open } = require('rosbag');
 const jpeg = require('jpeg-js');
+const http = require('http');
+const { Server } = require("socket.io");
+const { open } = require('rosbag');
+const { rejects } = require('assert');
 
-const server = express();
-const port = process.env.PORT || 8000;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const port = process.env.PORT || 3000;
 
 // ROUTES
 
-server.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + "/routes/index.html");
 });
 
-server.get("/bag", (req, res) => {
-    logMessagesFromFooBar("ros-1").then((data) => {
-        res.send("Succesfull");
-    })
-    .catch((error) => {
-        res.send(error);
-    })
+// SOCKET.IO
+
+io.on('connection', (socket) => {
+    console.log("a user connected");
+
+    socket.on('clicked', (msg, callback) => {
+        extract_from_bagFile(msg)
+        .then(() => {
+            callback("Succesfull");
+        })
+        .catch((error) => {
+            console.log(error);
+            callback("Error: " + error);
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
+
+
 
 // CONNECTION
   
@@ -28,9 +48,17 @@ server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
 
+/*async function extract() {
+    await extract_images_from_bagFile("ros-1")
+        .then((value) => {
+            console.log("VAL " + value);
+        });
+    console.log("STOP");
+}*/
+
 // FUNCTIONS
 
-async function logMessagesFromFooBar(bagFile) {
+async function extract_from_bagFile(bagFile) {
     const bag = await open(__dirname + `/public/bagFile/${bagFile}.bag`)
 
     var topics = [];    // All topics in the bag file
@@ -50,69 +78,78 @@ async function logMessagesFromFooBar(bagFile) {
         if (access_error) {
             fs.mkdir(__dirname + `/public/bagFile/${bagFile}`, (create_error) => {
                 if (create_error) {
-                    console.info(`Error on create folder ${bagFile} with the following error`);
+                    console.warn(`Error on create folder ${bagFile} with the following error`);
                     console.error(create_error);
                 } else {
                     console.info(`Folder ${bagFile} created`);
                 }
             });
         } else {
-            console.info(`Folder ${bagFile} already exist`);
+            console.warn(`Folder ${bagFile} already exist`);
         }
     });
 
+
     // if empty, by default it reads all topics
     await bag.readMessages({}, (result) => {
-            
-            //console.log("Campo message: " + util.inspect(result, {depth: 5, colors: true, compact: true}));
+        //console.log("Campo message: " + util.inspect(result, {depth: 5, colors: true, compact: true}));
 
-            // calculate the percentage of progress
-            var total = parseInt(result.chunkOffset / result.totalChunks * 100);
-            if (total > percentage) {
-                percentage = total;
-                console.log(percentage + "%");
-            }
-            
-            if (result.message.data != undefined && result.message.encoding != undefined) {
+        // calculate the percentage of progress
+        var total = parseInt(result.chunkOffset / result.totalChunks * 100);
+        if (total > percentage) {
+            percentage = total;
+            console.log(percentage + "%");
+        }
+        
+        if (result.message.data != undefined && result.message.encoding != undefined) {
 
-                var formatted_topic = result.topic.split('/').join('_');
-                
-                // Check if for each topics there is already a folder where you can save the foto
-                try {
-                    fs.accessSync(__dirname + `/public/bagFile/${bagFile}/${formatted_topic}`, fs.constants.F_OK);
-                } catch (access_error) {
-                    fs.mkdir(__dirname + `/public/bagFile/${bagFile}/${formatted_topic}`, (create_error) => {
-                        if (create_error) {
-                            console.info(`Error on create folder ${formatted_topic} with the following error`)
-                            console.error(create_error);
-                        }
-                    });
-                }
-                
-                // Check if the image already exist, do not overwrite and skip the encode to prevent time
-                try {
-                    fs.accessSync(__dirname + `/public/bagFile/${bagFile}/${formatted_topic}/img-${result.message.header.seq}.png`, fs.constants.F_OK);
-                } catch (access_error) {
-                    var rawImageData = {
-                        data: result.message.data,
-                        width: result.message.encoding == "32FC1" ? result.message.width : 240, //doesn't works with result.message.width for rgb images
-                        height: result.message.height
-                    };
-                    var jpegImageData = jpeg.encode(rawImageData, 0);
-    
-                    try {
-                        fs.writeFileSync(__dirname + `/public/bagFile/${bagFile}/${formatted_topic}/img-${result.message.header.seq}.png`, jpegImageData.data);
-                    } catch(error) {
-                        console.info(`Error on create image ${formatted_topic}/img-${result.message.header.seq}.png with the following error`);
-                        console.error(error);
+            var formatted_topic = result.topic.split('/').join('_');
+            
+            // Check if for each topics there is already a folder where you can save the foto
+            try {
+                fs.accessSync(__dirname + `/public/bagFile/${bagFile}/${formatted_topic}`, fs.constants.F_OK);
+            } catch (access_error) {
+                fs.mkdir(__dirname + `/public/bagFile/${bagFile}/${formatted_topic}`, (create_error) => {
+                    if (create_error) {
+                        console.warn(`Error on create folder ${formatted_topic} with the following error`)
+                        console.error(create_error);
                     }
-                }
+                });
             }
             
-            // Save all the image encoding 
-            if (!encodings.includes(result.message.encoding))
-                encodings.push(result.message.encoding);
+            // Check if the image already exist, do not overwrite and skip the encode to prevent time
+            try {
+                fs.accessSync(__dirname + `/public/bagFile/${bagFile}/${formatted_topic}/img-${result.message.header.seq}.png`, fs.constants.F_OK);
+            } catch (access_error) {
+                var rawImageData = {
+                    data: result.message.data,
+                    width: result.message.encoding == "32FC1" ? result.message.width : 240, //doesn't works with result.message.width for rgb images
+                    height: result.message.height
+                };
+                var jpegImageData = jpeg.encode(rawImageData, 0);
+
+                try {
+                    fs.writeFileSync(__dirname + `/public/bagFile/${bagFile}/${formatted_topic}/img-${result.message.header.seq}.png`, jpegImageData.data);
+                } catch(error) {
+                    console.warn(`Error on create image ${formatted_topic}/img-${result.message.header.seq}.png with the following error`);
+                    console.error(error);
+                }
+            }
+        }
+        
+        // Save all the image encoding 
+        if (!encodings.includes(result.message.encoding))
+            encodings.push(result.message.encoding);
     });
+
     console.log(topics);
     console.log(encodings);
+
+    /*return new Promise(() => {
+        let interval = setInterval(() => {
+            console.log("P " + percentage);
+            if (percentage >= 99)
+                clearInterval(interval);
+        }, 1000);
+    });*/
 }
